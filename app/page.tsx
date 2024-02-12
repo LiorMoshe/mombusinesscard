e4a6b8bee94e246e8c09efcbd9027b2b6af4323d
LiorMoshe <liormoshe0909@gmail.com>
2024-02-12 22:37:31 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,111 @@
-import Image from "next/image";
+// import Image from "next/image";
+// import BusinessCard from "./business-card"
+'use client'
+import Head from 'next/head'
+import styles from '../styles/BusinessCard.module.css'
+import Image from 'next/image' // Importing the Image component from Next.js
+import Link from 'next/link'
+
 
 export default function Home() {
+
+  const openWhatsAppChat = () => {
+    const phoneNumber = "+972548198124";
+    const message = encodeURIComponent("היי, אשמח לתאם איתך שיחה");
+    window.open(`https://wa.me/${phoneNumber}?text=${message}`, '_blank');
+  };
+
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+      <Head >
+        <title >Karen Moshe - Lawyer</title>
+        <meta name="description" content="Digital Business Card for Karen Moshe, Lawyer" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <link href="https://fonts.googleapis.com/css2?family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+      </Head>
+
+      <div className={styles.card}>
+        <div className={styles.logoContainer}>
+          {/* SET THE CSS FOR ME TO BE CENTERED, AT THE CENTER AND HAVE WIDTH TWICE AS HEIGHT SINCE IM WIDE */}
+          <Image src="/flat_logo.png" alt="Flat Logo" width={240} height={120} />
+        </div>
+        <div className={styles.profilePic}>
+          {/* Replace '/path/to/profile-pic.jpg' with the path to your mom's profile picture */}
+          <Image src="/mom_pic.png" alt="Profile Picture" width={120} height={120} className={styles.profileImage} />
+        </div>
+        <div className={styles.header}>
+          <h1>קארן משה</h1>
+          <p>עו״ד ומגשרת
+            <br />
+            לענייני משפחה וירושה </p>
+        </div>
+
+        <div className={styles.iconContainer} >
+          <div className={`${styles.iconFlexContainer} ${styles.whatsapp}`}>
+            <div className={styles.icon} onClick={openWhatsAppChat}>
+              <img src="/whatsapp.svg" alt="whatsapp" width="80" height="80" />
+            </div>
+            <span className={styles.iconLabel}>whatsapp</span>
+          </div>
+
+          <div className={`${styles.iconFlexContainer} ${styles.email}`}>
+            <a href="mailto:law@kmoshe.com?subject=אשמח לעזרה">
+
+              <div className={styles.icon}>
+                <img src="/email.svg" alt="email" width="80" height="80" />
+              </div>
+            </a>
+            <span className={styles.iconLabel}>מייל</span>
+          </div>
+
+          <div className={`${styles.iconFlexContainer} ${styles.website}`}>
+            <a href="https://www.karenmoshelaw.com" target="_blank" rel="noopener noreferrer">
+              <div className={styles.icon}>
+                <img src="/website.svg" alt="website" width="80" height="80" />
+              </div>
+            </a>
+            <span className={styles.iconLabel}>אתר</span>
+          </div>
+
+          <div className={`${styles.iconFlexContainer} ${styles.facebook}`}>
+            <a href="https://www.facebook.com/profile.php?id=61556164628730" target="_blank" rel="noopener noreferrer">
+              <div className={styles.icon}>
+                <img src="/facebook.svg" alt="Facebook" width="80" height="70" />
+              </div>
+            </a>
+            <span className={styles.iconLabel}>פייסבוק</span>
+          </div>
+
+          <div className={`${styles.iconFlexContainer} ${styles.phone}`}>
+            <a href="tel:+972548198124">
+              <div className={styles.icon}>
+                <img src="/phone.svg" alt="phone" width="80" height="80" />
+              </div>
+              <span className={styles.iconLabel}>טלפון</span>
+            </a>
+          </div>
+
+          <div className={`${styles.iconFlexContainer} ${styles.waze}`}>
+            <a href="https://maps.app.goo.gl/LDE6ej3Ye6bp7reT6" target="_blank">
+              <div className={styles.icon}>
+                <img src="/maps.svg" alt="maps" width="80" height="80" />
+              </div>
+            </a>
+            <span className={styles.iconLabel}>מיקום</span>
+          </div>
+
+
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
